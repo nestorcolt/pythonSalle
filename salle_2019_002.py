f934e87c7b07f8a11226bec8b4d123858f4c873f
nestorcolt <nestorcolt@hotmail.com>
2019-05-19 21:50:11 +0200

    class week 2 done

--- salle_2019_002.py
+++ salle_2019_002.py
@@ -63,7 +63,7 @@ def save_file(path="",file_name="", content=None):
     if not os.path.exists(path):
         sys.stdout.write("Error: {} is not a valid path".format(path))
         return
-    elif content is  None:
+    elif content is None:
         sys.stdout.write("Error: content to save is empty")
         return
 
@@ -131,5 +131,91 @@ def set_objects_matrix(object_data={}):
 # calling functions
 pos_data = get_objects_matrix(all_transform)
 set_objects_matrix(pos_data)
+
+######################################################################################################
+
+# Test what we know so far ...
+#
+def store_scene_object_transformation(object_list=[], os_path='', file_name=''):
+    pos_data = get_objects_matrix(object_list)
+    json_format = json.dumps(pos_data, separators=(',', ':'), indent=4)
+
+    if not os.path.exists(os_path):
+        try:
+            os.makedirs(os_path)
+        except:
+            sys.stdout.write("Error: couldn't create path")
+            return
+
+    # if path exists or creation successful save the file
+    save_file(path=os_path, file_name="{}.json".format(file_name), content=json_format)
+
+
+def restore_scene_object_transformation(os_path='', file_name=''):
+    data = read_file(path=os_path, file_name="{}.json".format(file_name))
+    obj_pos_data = json.loads(data)
+    set_objects_matrix(obj_pos_data)
+
+######################################################################################################
+# do it!
+all_transform = mc.ls(type="transform")
+PATH = os.path.join(USER_PROFILE, "Documents", "maya", "positionLib")
+FILE_NAME = "scene_001"
+store_scene_object_transformation(object_list=all_transform, os_path=PATH, file_name=FILE_NAME)
+#
+#
+restore_scene_object_transformation(os_path=PATH, file_name=FILE_NAME)
+
+######################################################################################################
+# Part II : Maya commands UI development
+#
+def ui_initialize(data_path = ""):
+    if not data_path or not os.path.exists(data_path):
+        mc.error("There is a problem with the path provided, please check it and try again")
+        return
+
+    def on_save(widget=""):
+        all_transform = mc.ls(type="transform")
+        file_name = mc.textField(widget, q=True, text=True)
+        #
+        if not file_name:
+            return
+        #
+        store_scene_object_transformation(object_list=all_transform, os_path=data_path, file_name=file_name)
+        mc.textField(widget, edit=True, text="")
+
+    def on_load(widget=''):
+        current_item = mc.optionMenu(widget, q=True, value=True)
+        if not current_item:
+            return
+        #
+        file_name = current_item.replace(".json", "")
+        restore_scene_object_transformation(os_path=PATH, file_name=file_name)
+
+
+    win_name = "Sample_window"
+
+    if mc.window(win_name, exists=True):
+        mc.deleteUI(win_name)
+
+    window = mc.window(win_name, title="Scene Snapshot Tool", sizeable=True, width=400, retain=False, resizeToFitChildren=True)
+    layout = mc.columnLayout(adjustableColumn=True, parent=window, rowSpacing=15)
+    mc.text(label="Path To Store Data:", p=layout, align="left", font="boldLabelFont")
+    mc.textField(p=layout, text=data_path, editable=False)
+    mc.text(label="Save Version:", p=layout, align="left", font="boldLabelFont")
+    ver_widget = mc.textField(p=layout, editable=True)
+    save_btn = mc.button("Save", parent=layout, command=lambda x: on_save(widget=ver_widget))
+    #
+    opt_menu = mc.optionMenu("UI_version_loader_menu", w=250, label="Load Version:", parent=layout)
+
+    for item in os.listdir(data_path):
+        mc.menuItem(label=item, p=opt_menu)
+
+    load_btn  = mc.button("Load", parent=layout, command=lambda x: on_load(widget=opt_menu))
+
+    # show the UI window now
+    mc.showWindow(window)
+
+ui_initialize(data_path=PATH)
 
 ######################################################################################################
